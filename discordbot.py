01497e87340625c8ab6b9e5833eb658a55ce4eba
muji-jun <105193914+muji-jun@users.noreply.github.com>
2023-01-12 07:52:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -8,25 +8,36 @@ load_dotenv()
 PREFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
-client = discord.Client()
+def main():
+    intents = discord.Intents.all()
+    client = commands.Bot(command_prefix='>', intents=intents)
+    load_dotenv()
 
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user}.')
+    @client.event
+    async def on_ready():
+        print("다음으로 로그인합니다 : ")
+        print(client.user.name)
+        print(client.user.id)
+        print("==========")
+        game = discord.Game("정상가동")
+        await client.change_presence(status=discord.Status.online, activity=game)    
+    
+    #모듈
+    @client.event
+    async def setup_hook():
+        for folder in os.listdir("modules"):
+            if os.path.exists(os.path.join("modules", folder, "cog.py")) :
+                await client.load_extension(f"modules.{folder}.cog")
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    #토큰
+    f = open('token.txt', 'r')
+    token = f.readline()
 
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
+    #봇 가동
+    try:
+        client.run(TOKEN)
+    except discord.errors.LoginFailure as e:
+        print("Improper token has been passed.") 
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
-
-
-try:
-    client.run(TOKEN)
-except discord.errors.LoginFailure as e:
-    print("Improper token has been passed.")
+if __name__ == '__main__':
+    main()    
